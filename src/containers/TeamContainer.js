51dddfb3986e8706a265816bfd9d66d33ca19a9c
tt-p <tt-p@github.com>
2021-10-18 21:53:56 +0300

    add team members page

---
+++ src/containers/TeamContainer.js
@@ -0,0 +1,34 @@
+import {Grid} from "@mui/material";
+import TeamMember from "../components/TeamMember";
+import {useState} from "react";
+import {TeamMembersList} from "../info"
+
+const TeamContainer = () => {
+
+    const [memberList, updateMemberList] = useState(TeamMembersList);
+
+    return (
+        <Grid
+            container
+            direction="row"
+            justifyContent="center"
+            alignItems="center"
+            spacing={1}
+        >
+
+            {
+                memberList.map(member => {
+                    return (
+                        <Grid key={member.id} item xs={12} sm={6} md={4} lg={3}>
+                            <TeamMember member={member} />
+                        </Grid>
+                    )
+                })
+            }
+
+        </Grid>
+    );
+
+}
+
+export default TeamContainer;
